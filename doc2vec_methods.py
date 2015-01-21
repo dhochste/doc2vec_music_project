1bd589213a301639b5188a50621d9fa520546ff4
David Hochstetler <dlhochstetler@gmail.com>
2015-01-20 23:09:10 -0800

    Added load_pickled_df, df_colmumn_reduce, df_title_format, df_review_collapse

--- doc2vec_methods.py
+++ doc2vec_methods.py
@@ -17,7 +17,53 @@ import pandas as pd
 import numexpr as ne 		# Used by pandas.query method
 import sys
 import re
-import gensim, logging
+import gensim, logging 		# For Doc2Vec
 import cPickle as pickle 	# Used for pickling/unpickling files
+
+
+def load_pickled_df(directory_path, file_path):
+    """
+    Load the df from pickled pandas dataframe
+    """
+
+    file_0 = directory_path+file_path
+
+    with open(file_0, 'rb') as pickle_file:
+        df = pickle.load(pickle_file)
+
+    return df
+
+def df_column_reduce(df):
+	"""
+	Keep only relevant columns. Using hard-coded column names from Amazon database
+	"""
+	df = df.drop(['product/price','review/helpfulness','review/score','review/profileName'], \
+		axis=1, inPlace=False)
+
+	df.columns = ['productId','title','summary','time','userId','tokenize']
+
+	return df
+
+def df_title_format(df):
+	"""
+	Replace spaces with underscores
+	"""
+	for i in range(len(df)):
+		df['title'][i] = df_mini_gb['title'][i].replace(' ','_')
+
+	return df
+
+def df_review_colapse(df):
+	"""
+	Apply aggregation so there is only 1 row per review (append the tokenize column 
+	for previous multiple lines of df for a single review)
+	"""
+	df = df.groupby(list(df.columns[:-1])).agg({'tokenize':'sum'})
+	df.reset_index(inplace=True)
+
+	return df
+
+
+
 
 
